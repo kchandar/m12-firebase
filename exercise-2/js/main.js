f7d19802575f82e3fa69dd8f36df3993c24a7e79
Keertana Chandar <kchandar@uw.edu>
2016-10-29 18:31:24 -0700

    exercises

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -2,39 +2,61 @@
 $(function() {
 
 	// Setup: Initialize Firebase using the configuration of your project
-
+	var config = {
+		apiKey: "AIzaSyAQDxm4Ne3Pwz4YWnq5afyPjUPRr4_BKFA",
+		authDomain: "test-project-ce663.firebaseapp.com",
+		databaseURL: "https://test-project-ce663.firebaseio.com",
+		storageBucket: "test-project-ce663.appspot.com",
+		messagingSenderId: "982602873791"
+	};
+	firebase.initializeApp(config);
 
 	// Set a reference to a "photos" point in your database
-
+	var photos = firebase.database().ref('photos');
 
 	// Create a variable to store the firebase storage object
+	var storage = firebase.storage();
+
+
 
 
 	// Set listener: when an child is added, render each photo
+	photos.on('child_added', function(snapshot) {
 
 		// Get the value of the data
-
+		var data = snapshot.val();
 
 		// Using jQuery, create a new img element with the URL of your data
+		var img = $('<img>').attr('src', data.url)
 
 
 		// Append your img to your element with id photos
+		$('#photos').append(img);
+	});
 
 
 
 
 	// Reading Data: Form submission
-
+	$('form').on('submit', function(event) {
+		event.preventDefault();
 		// Get the file
-
+		var file = $("#file-upload")[0].files[0];
 
 		// Create a reference on Firebase storage using the filename
-
+		var fileRef = storage.ref(file.name);
+		console.log(file);
+		console.log(file.name);
 
 		// Put a file in the specified location, then...
-
+		fileRef.put(file).then(function(){
 			// Get the download URL from the reference, then...
-
+			fileRef.getDownloadURL().then(function(url) {
 				// Push the URL as a new child into your data structure
-
-		
+				photos.push({
+					url: url
+				});
+			});
+		});
+	});
+});
